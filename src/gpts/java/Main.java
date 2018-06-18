0285d3ba6cac0e95a078397292acada5aa3b392a
thejrace <ahmetkanbur@gmail.com>
2018-06-18 15:15:58 +0300

    dailyplan tests

--- src/gpts/java/Main.java
+++ src/gpts/java/Main.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.sql.Time;
+
 public class Main extends Application {
 
     // we use this access it from MainController
@@ -25,6 +27,40 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(content, 1224, 768));
         stage = primaryStage;
         primaryStage.show();
+
+        String start = "08:00";
+        String end   = "17:00";
+        int interval = 70;
+        String[] from_exp = start.split(":");
+        String[] to_exp = end.split(":");
+        int from_saat = Integer.valueOf(from_exp[0]);
+        int to_saat = Integer.valueOf( to_exp[0] );
+        int from_dk = Integer.valueOf( from_exp[1] );
+        int to_dk = Integer.valueOf( to_exp[1] );
+
+        int next_hour = from_saat,
+                next_min = from_dk;
+        String prev_hour_str = start;
+        String next_hour_str;
+        for( int k = from_saat; k < to_saat; k++ ){
+            next_min += interval;
+            if( next_min >= 60 ){
+                next_hour++;
+                if( next_hour == to_saat && next_min > from_dk ){
+                    next_min = from_dk;
+                }
+                next_hour_str =  String.valueOf(next_hour) + ":" + ( next_min % 60 );
+
+            } else {
+                next_hour_str =  String.valueOf(next_hour) + ":" + next_min;
+            }
+            System.out.println( prev_hour_str + "  -  " + next_hour_str );
+            prev_hour_str = next_hour_str;
+        }
+
+
+
+
 
 
     }
