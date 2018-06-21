1da10473fca627062e63e87548d71ab0e1feb945
thejrace <ahmetkanbur@gmail.com>
2018-06-21 23:17:18 +0300

    updated to new BasePage format

--- src/gpts/java/ui/EmployeesPage.java
+++ src/gpts/java/ui/EmployeesPage.java
@@ -6,7 +6,6 @@ import gpts.java.WebRequest;
 import gpts.java.controllers.EmployeesController;
 import gpts.java.interfaces.WebRequestCallback;
 import javafx.application.Platform;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -14,8 +13,9 @@ import java.util.Map;
 
 public class EmployeesPage extends BasePage{
 
-    private boolean mDownloadThreadActive = true, mDataDownloadInited;
-    private Map<String, EmpBox> mEmpBoxes = new HashMap<>();
+    // Rows holder lists
+    protected Map<String, EmpBox> mRows = new HashMap<>();
+    protected Map<String, EmpBox> mSearchRows = new HashMap<>();
     private EmployeesController mController;
 
     public EmployeesPage(  ){
@@ -26,74 +26,100 @@ public class EmployeesPage extends BasePage{
     public void initUI( String fxml ){
         super.initUI(fxml);
         mController = (EmployeesController)mBaseController;
-        startDownloadThread();
+        downloadData();
+        mController.setPageObject( this );
     }
 
-    public void startDownloadThread(){
-        Map<String, String> downloadParams = new HashMap<>();
-        downloadParams.put("req", "employees_download");
-        Thread downloadThread = new Thread(new Runnable() {
+    /* we show limited data on intialize, if user wants to see more
+     *  clicks to download more button
+     */
+    public void downloadData(){
+        // if download more data, show loader
+        // first download loader inited from planscontroller
+        if( mDownloadInited ) PopupLoader.show("Veri alınıyor..");
+        if( !mDownloadInited ) mDownloadInited = true;
+        mController.disableMoreBtn( false );
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while( mDownloadThreadActive ){
-                    // download data
-                    WebRequest request = new WebRequest(WebRequest.SERVICE_URL, downloadParams );
-                    request.action(new WebRequestCallback() {
-                        @Override
-                        public void onFinish(JSONObject output) {
-                            JSONArray list = output.getJSONArray("data");
-                            JSONObject tempData;
-                            // first download and initialize boxes
-                            if( !mDataDownloadInited ){
-                                for( int k = 0; k < list.length(); k++ ){
-                                    tempData = list.getJSONObject(k);
-                                    final EmpBox empBox = new EmpBox( new Employee( tempData ) );
-                                    mEmpBoxes.put( tempData.getString("id"), empBox );
-                                    Platform.runLater(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            mController.addEmpBox( empBox.getUI() );
-                                        }
-                                    });
-                                }
-                                mDataDownloadInited = true;
-                                Platform.runLater(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        PopupLoader.hide();
-                                    }
-                                });
-                            } else {
-                                // boxes initalized, update them
-                                for( int k = 0; k < list.length(); k++ ){
-                                    tempData = list.getJSONObject(k);
-                                    try {
-                                        EmpBox boxTemp = mEmpBoxes.get( tempData.getString("id"));
-                                        boxTemp.updateData( new Employee( tempData ) );
-                                        Platform.runLater(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                boxTemp.updateUI();
-                                            }
-                                        });
-                                    } catch( NullPointerException e ){
-                                        e.printStackTrace();
-                                    }
+                Map<String, String> downloadParams = new HashMap<>();
+                if( mSearchFlag ){
+                    downloadParams.put("req", "employees_search");
+                    downloadParams.put("keyword", mSearchKeyword );
+                } else {
+                    downloadParams.put("req", "employees_download");
+                }
+                downloadParams.put("start_index", String.valueOf(mLastItemIndex));
+                downloadParams.put("rrp", String.valueOf(mRRP));
+                WebRequest request = new WebRequest(WebRequest.SERVICE_URL, downloadParams );
+                request.action(new WebRequestCallback() {
+                    @Override
+                    public void onFinish(JSONObject output) {
+                        mData = output.getJSONArray("data");
+                        addItems(new BasePage.AddItemCallback() {
+                            @Override
+                            public void action( int length ) {
+                                // if last downloaded data is less than RRP means that
+                                // there aren't any data left to download
+                                // therefore we disable the button to avoid user making useless requests
+                                if( length <= mRRP ) mController.disableMoreBtn( true );
+                                JSONObject temp;
+                                for( int k = 0; k < length; k++ ){
+                                    temp = mData.getJSONObject(k);
+                                    EmpBox row = new EmpBox( new Employee( temp ));
+                                    //PlanDataRow row = new PlanDataRow( new DailyPlanSchema( temp.getString("id"), temp.getString("name"), temp.getString("start"), temp.getString("end"), temp.getString("plan_interval") ));
+                                    addItem( temp.getString("name"), row, false );
                                 }
                             }
-                        }
-                    });
-                    try {
-                        // todo configden alinacak
-                        Thread.sleep( 60000 );
-                    } catch( InterruptedException e ){
-                        e.printStackTrace();
+                        });
                     }
-                }
+                });
             }
         });
-        downloadThread.setDaemon(true);
-        downloadThread.start();
+        thread.setDaemon(true);
+        thread.start();
     }
+
+
+    /* add single item
+     *   @key  : mRows key
+     *   @row  : ui object
+     *   @sort : if true, datarows sorted according to their node ID's ( names for this case )
+     */
+    public void addItem( String key, EmpBox row, boolean sort ){
+        if( mSearchFlag ){
+            mSearchRows.put( key, row );
+        } else {
+            mRows.put( key, row );
+        }
+        Platform.runLater( () -> { mController.addRow( row.getUI(), sort); } );
+    }
+
+    @Override
+    public void search( String keyword ){
+        // if user makes search repeateadly, we avoid overwriting
+        // first state's moreBtton state, that's why we only save it
+        // if no search flag is set
+        if( !mSearchFlag ) mController.saveMoreBtnState();
+        super.search( keyword );
+        clearItems();
+        downloadData();
+    }
+
+    // remove all datarows
+    private void clearItems(){
+        Platform.runLater(()->{
+            mController.clearItems();
+        });
+    }
+
+    // cancel search return first state
+    public void cancelSearch(){
+        super.cancelSearch();
+        mController.restoreFirstState();
+    }
+
+
+
 
 }
